d9de11bf0486d1f5cf3849503dd0d0196dde1288
JOAO GUILHERME PEREIRA <joaoguip006@gmail.com>
2026-02-06 16:05:29 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,27 +7,29 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// CONFIGURAÇÃO DO SEU BANCO (Visto no seu print do Workbench)
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Verifique se sua senha do MySQL não é vazia
-    password: '', 
-    database: 'projeto_geek'
+   
+    host: process.env.MYSQLHOST || 'localhost',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || '', 
+    database: process.env.MYSQLDATABASE || 'projeto_geek',
+    port: process.env.MYSQLPORT || 3306
 });
 
 db.connect(err => {
     if (err) {
-        console.error('ERRO AO CONECTAR NO MYSQL:', err);
+        console.error('ERRO AO CONECTAR NO BANCO:', err);
     } else {
-        console.log('ESTRUTURA PRONTA: BANCO CONECTADO COM SUCESSO!');
+        console.log('SISTEMA ONLINE: CONEXÃO COM BANCO ESTABELECIDA!');
     }
 });
 
-// ROTA PARA SALVAR OS DADOS
+
 app.post('/salvar', (req, res) => {
     const { nome, idade, genero, personagem, feedback } = req.body;
     
-    // O nome da tabela é 'cadastros', como no seu print
+    
     const sql = "INSERT INTO cadastros (nome, idade, genero, resultado_personagem, feedback) VALUES (?, ?, ?, ?, ?)";
     
     db.query(sql, [nome, idade, genero, personagem, feedback], (err, result) => {
@@ -35,12 +37,14 @@ app.post('/salvar', (req, res) => {
             console.error("ERRO AO INSERIR NO BANCO:", err);
             res.status(500).send("Erro ao salvar dados");
         } else {
-            console.log("SUCESSO: Novo herói registrado no banco!");
+            console.log("SUCESSO: Novo registro inserido!");
             res.status(200).send("Salvo com sucesso");
         }
     });
 });
 
-app.listen(3000, () => {
-    console.log('SERVIDOR RODANDO NA PORTA 3000');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`SERVIDOR RODANDO NA PORTA ${PORT}`);
 });
